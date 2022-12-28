cc9bdc05860dcf240da50b1e0a6e2dc88f1a57b4
Mike McGann <mike.mcgann@blackchip.org>
2022-12-28 14:50:05 -0500

    with history

--- app/console.go
+++ app/console.go
@@ -3,6 +3,8 @@ package app
 import (
 	"fmt"
 	"log"
+	"os"
+	"path"
 	"strings"
 
 	"github.com/blackchip-org/zc"
@@ -16,6 +18,9 @@ func RunConsole(calc *zc.Calc) {
 	line := liner.NewLiner()
 	defer line.Close()
 
+	loadHistory(line)
+	defer saveHistory(line)
+
 	line.SetCtrlCAborts(true)
 	line.SetTabCompletionStyle(liner.TabPrints)
 
@@ -28,27 +33,6 @@ func RunConsole(calc *zc.Calc) {
 		log.Printf("error: %v", err)
 		return
 	}
-
-	// homedir, err := os.UserHomeDir()
-	// if err != nil {
-	// 	log.Fatalf("unable to determine home directory: %v", err)
-	// }
-
-	// historyFile := path.Join(homedir, ".zc-history")
-	// f, err := os.OpenFile(historyFile, os.O_CREATE, 0o640)
-	// if err != nil {
-	// 	log.Printf("unable to save history: %v", err)
-	// 	return
-	// }
-
-	// defer func() {
-	// 	fmt.Println("****** SAVING HISTORY")
-	// 	_, err := line.WriteHistory(f)
-	// 	if err != nil {
-	// 		log.Printf("unable to save history: %v", err)
-	// 	}
-	// 	f.Close()
-	// }()
 
 	for ; err == nil; text, err = line.Prompt(prompt) {
 		var err error
@@ -96,5 +80,50 @@ func RunConsole(calc *zc.Calc) {
 		if strings.TrimSpace(text) != "" {
 			line.AppendHistory(text)
 		}
+	}
+}
+
+func loadHistory(line *liner.State) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("unable to determine home directory: %v", err)
+		return
+	}
+
+	historyFile := path.Join(homedir, ".local", "share", "zc", "history")
+	file, err := os.Open(historyFile)
+	if err != nil {
+		log.Printf("unable to load history: %v", err)
+		return
+	}
+	defer file.Close()
+	if _, err := line.ReadHistory(file); err != nil {
+		log.Printf("unable to load history: %v", err)
+	}
+}
+
+func saveHistory(line *liner.State) {
+	homedir, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("unable to determine home directory: %v", err)
+		return
+	}
+
+	localDir := path.Join(homedir, ".local", "share", "zc")
+	if err := os.MkdirAll(localDir, 0o700); err != nil {
+		log.Printf("unable to create local directory: %v", err)
+		return
+	}
+
+	historyFile := path.Join(localDir, "history")
+	file, err := os.OpenFile(historyFile, os.O_CREATE|os.O_WRONLY, 0o640)
+	if err != nil {
+		log.Printf("unable to save history: %v", err)
+		return
+	}
+
+	_, err = line.WriteHistory(file)
+	if err != nil {
+		log.Printf("unable to save history: %v", err)
 	}
 }
