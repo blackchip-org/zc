f898436d92dcf1b187208f2ee327b2d879d3b627
Mike McGann <mike.mcgann@blackchip.org>
2023-04-26 17:24:53 -0400

    add toc

---
+++ pkg/doc/categories.go
@@ -0,0 +1,38 @@
+package doc
+
+type Category struct {
+	Name   string
+	Groups []string
+}
+
+var Categories = []Category{
+	{"Calculator Operations", []string{
+		"basic",
+		"prog",
+		"sci",
+		"stat",
+	}},
+	{"Units of Measure", []string{
+		"len",
+		"si",
+		"temp",
+	}},
+	{"Standard Operations", []string{
+		"bool",
+		"cmp",
+		"format",
+		"hof",
+		"stack",
+		"text",
+		"types",
+	}},
+	{"Library", []string{
+		"color",
+		"crypto",
+		"rand",
+		"time",
+	}},
+	{"Tables", []string{
+		"tz",
+	}},
+}
