e2159dd26b49264374950cebb4aa67aa564c91e3
Mike McGann <mike.mcgann@blackchip.org>
2024-02-06 09:24:57 -0500

    update test ops

--- test/ops_test.go
+++ test/ops_test.go
@@ -6,10 +6,10 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/blackchip-org/scan"
 	"github.com/blackchip-org/zc/v5/pkg/ansi"
 	"github.com/blackchip-org/zc/v5/pkg/calc"
 	"github.com/blackchip-org/zc/v5/pkg/repl"
-	"github.com/blackchip-org/zc/v5/pkg/scanner"
 	"github.com/blackchip-org/zc/v5/pkg/zc"
 )
 
@@ -47,39 +47,47 @@ func testFile(t *testing.T, file string) {
 	}
 	defer f.Close()
 
-	s := scanner.New(f)
+	s := scan.NewScanner(file, f)
 
 	var blockName string
-	for s.Ok() {
-		if s.Ch == '#' && s.Lookahead == '#' {
-			s.Next()
-			s.Next()
-			blockName = s.Scan(scanner.LineTrimSpace)
+	for s.HasMore() {
+		if s.This == '#' && s.Next == '#' {
+			s.Discard()
+			s.Discard()
+			scan.Until(s, scan.Rune('\n'), s.Keep)
+			blockName = strings.TrimSpace(s.Emit().Val)
+			s.Discard()
 		} else {
-			space := s.ScanWhile(scanner.Rune(' '))
+			scan.While(s, scan.Rune(' '), s.Keep)
+			space := s.Emit().Val
 			if space == space4 {
 				t.Run(blockName, func(t *testing.T) {
 					testBlock(t, s)
 				})
 			} else {
-				s.Scan(scanner.Line)
+				scan.Until(s, scan.Rune('\n'), s.Discard)
+				s.Discard()
 			}
 		}
 	}
 }
 
-func testBlock(t *testing.T, s *scanner.Scanner) {
+func testBlock(t *testing.T, s *scan.Scanner) {
 	c := calc.New()
 	r := repl.New(c)
 	out := &strings.Builder{}
 	r.Out = out
 
-	for s.Ok() {
-		test := s.Scan(scanner.LineTrimSpace)
+	for s.HasMore() {
+		scan.Until(s, scan.Rune('\n'), s.Keep)
+		test := strings.TrimSpace(s.Emit().Val)
+		s.Discard()
+
 		if !repl.Test(r, test) {
 			t.Errorf("\n output: %v (error %v) \n  input: %v\n", zc.StackString(c), c.Error(), test)
 		}
-		space := s.ScanWhile(scanner.Rune(' '))
+		scan.While(s, scan.Rune(' '), s.Keep)
+		space := s.Emit().Val
 		if space != space4 {
 			break
 		}
