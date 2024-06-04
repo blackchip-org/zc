986973c72d6a995db88da17911398fe34672a1cb
Mike McGann <mike.mcgann@blackchip.org>
2024-06-04 07:37:47 -0400

    typo, heading

--- internal/gen-ops/gen-ops.go
+++ internal/gen-ops/gen-ops.go
@@ -277,6 +277,7 @@ func genOpDoc(f *os.File, op zc.OpDef) {
 		fmt.Fprintln(f, "")
 	}
 	if op.Macro != "" {
+		fmt.Fprintf(f, "Macro definition:\n```\n")
 		fmt.Fprintf(f, "```\n")
 		fmt.Fprintf(f, "def %v %v\n", op.Name, op.Macro)
 		fmt.Fprintf(f, "```\n")
