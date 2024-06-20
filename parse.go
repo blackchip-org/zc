32fd7785fcb0490e58a25e1a1b19e63cb0d4585f
Mike McGann <mike.mcgann@blackchip.org>
2024-06-20 13:06:36 -0400

    add tests for pre-parsing

--- parse.go
+++ parse.go
@@ -3,7 +3,60 @@ package zc
 import (
 	"strings"
 	"unicode"
+
+	"github.com/blackchip-org/scan"
 )
+
+const (
+	TokenValue = "value"
+	TokenName  = "name"
+)
+
+type valueRule struct{}
+
+func (r valueRule) Eval(s *scan.Scanner) bool {
+	if !IsValuePrefix(s.This, s.Next) {
+		return false
+	}
+	s.Type = TokenValue
+	s.Keep()
+	scan.While(s, scan.Not(scan.IsSpace), s.Keep)
+	return true
+}
+
+type slashValueRule struct{}
+
+func (r slashValueRule) Eval(s *scan.Scanner) bool {
+	if s.This != '/' {
+		return false
+	}
+	s.Type = TokenValue
+	s.Skip()
+	scan.While(s, scan.Not(scan.IsSpace), s.Keep)
+	return true
+}
+
+var rules = scan.NewRuleSet(
+	scan.SkipSpaceRule,
+	scan.StrDoubleQuoteRule.
+		WithType(TokenValue).
+		WithOptionalTerminator(true),
+	scan.StrSingleQuoteRule.WithType(TokenValue).
+		WithType(TokenValue).
+		WithOptionalTerminator(true),
+	scan.NewStrRule('[', ']').
+		WithType(TokenValue).
+		WithOptionalTerminator(true),
+	valueRule{},
+	slashValueRule{},
+	scan.NewWhileRule(scan.Not(scan.IsSpace), TokenName),
+).WithNoMatchFunc(scan.UnexpectedUntil(scan.IsSpace))
+
+func ScanWords(line string) []scan.Token {
+	s := scan.NewScannerFromString("", line)
+	runner := scan.NewRunner(s, rules)
+	return runner.All()
+}
 
 func isFormatting(ch rune) bool {
 	if ch == ',' || ch == '_' || ch == ' ' {
--- parse_test.go
+++ parse_test.go
@@ -23,3 +23,27 @@ func TestScanner(t *testing.T) {
 	}
 	scan.RunTests(t, rules, tests)
 }
+
+func TestPreParseNumber(t *testing.T) {
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"1234", "1234"},
+		{"1,234", "1234"},
+		{"1_234", "1234"},
+		{"1 234", "1234"},
+		{"$1234", "1234"},
+		{"1234×1045", "1234e45"},
+		{"1234x1045", "1234e45"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.in, func(t *testing.T) {
+			out := PreParseNumber(test.in)
+			if out != test.out {
+				t.Fatalf("\n have: %v \n want: %v", out, test.out)
+			}
+		})
+	}
+}
